df538bf425730a5319b7a5c98117ddb176d0d95c
Isabel Soares <44942260+isabelSoares@users.noreply.github.com>
2019-03-11 09:24:40 +0000

    Add files via upload

---
+++ proj1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    int N, M,linha[N][M];
+    FILE *f;
+    f =fopen(filename, "r");
+    if ((N < 2) && (M < 1)){ fprintf(stderr,"Invalid number");}
+    return 0;
+}
