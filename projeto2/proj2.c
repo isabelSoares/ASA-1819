82821476d7fc7d43af2ad458afe88cb4e9fcc037
isabelSoares <isabel.srsoares@gmail.com>
2019-05-08 08:35:15 +0100

    dfs lá para cortes min

--- projeto2/proj2.c
+++ projeto2/proj2.c
@@ -1,7 +1,4 @@
 
-
-//Relabel To Front copiado
-
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
@@ -109,6 +106,41 @@ void printMatrix(int ** M) {
     printf("\n");
   }
 }
+
+void DFS(int);
+int G[10][10],visited[10],n;    //n is no of vertices and graph is sorted in array G[10][10]
+ 
+void main()
+{
+    int i,j;
+    printf("Enter number of vertices:");
+   
+	scanf("%d",&n);
+ 
+    //read the adjecency matrix
+	printf("\nEnter adjecency matrix of the graph:");
+   
+	for(i=0;i<n;i++)
+       for(j=0;j<n;j++)
+			scanf("%d",&G[i][j]);
+ 
+    //visited is initialized to zero
+   for(i=0;i<n;i++)
+        visited[i]=0;
+ 
+    DFS(0);
+}
+ 
+void DFS(int i)
+{
+  int j;
+	printf("\n%d",i);
+    visited[i]=1;
+	
+	for(j=0;j<n;j++)
+       if(!visited[j]&&G[i][j]==1)
+            DFS(j);
+}
  
 int main(void) {
   int **flow, **capacities, i,j,k,l,o,d,c;
@@ -163,6 +195,26 @@ int main(void) {
  
   printf("Flows:\n");
   printMatrix(flow);
+
+
+  // DFS para cortes mínimos 
+  int m,y;
+  printf("Enter number of vertices:");
+   
+	scanf("%d",&n);
+ 
+    //read the adjecency matrix
+	printf("\nEnter adjecency matrix of the graph:");
+   
+	for(i=0;i<n;i++)
+       for(y=0;y<n;y++)
+			scanf("%d",&G[m][y]);
+ 
+    //visited is initialized to zero
+   for(m=0;m<n;m++)
+        visited[m]=0;
+ 
+    DFS(0);
  
   return 0;
 }
