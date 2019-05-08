7cc6c04a3c1a87a9b5f8f2dcdbf6918264f42776
isabelSoares <isabel.srsoares@gmail.com>
2019-05-08 09:31:40 +0100

    dfs

--- projeto2/proj2.c
+++ projeto2/proj2.c
@@ -107,29 +107,9 @@ void printMatrix(int ** M) {
   }
 }
 
-void DFS(int);
+
 int G[10][10],visited[10],n;    //n is no of vertices and graph is sorted in array G[10][10]
- 
-void main()
-{
-    int i,j;
-    printf("Enter number of vertices:");
-   
-	scanf("%d",&n);
- 
-    //read the adjecency matrix
-	printf("\nEnter adjecency matrix of the graph:");
-   
-	for(i=0;i<n;i++)
-       for(j=0;j<n;j++)
-			scanf("%d",&G[i][j]);
- 
-    //visited is initialized to zero
-   for(i=0;i<n;i++)
-        visited[i]=0;
- 
-    DFS(0);
-}
+
  
 void DFS(int i)
 {
